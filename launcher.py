cbefe3a0015d38acca2df1b62578500a1f7f15c5
DuskCipher <aprineads@gmail.com>
2025-06-02 14:45:40 +0700

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,13 +1,11 @@
 import subprocess
 import os
-import sys
 
-# Ambil path folder ini
+# Ambil path folder ini (PANEL/)
 base_dir = os.path.dirname(os.path.abspath(__file__))
-project_dir = os.path.join(base_dir, "WhatsPanel-main")
 
-# Jalankan streamlit app
-script_path = os.path.join(project_dir, "dashboard", "kirim_pesan.py")
+# Path ke file utama yang ingin dijalankan
+script_path = os.path.join(base_dir, "whatsapp_dashboard.py")
 
-# Jalankan streamlit sebagai proses baru
-subprocess.run(["streamlit", "run", script_path], shell=True)
+# Jalankan Streamlit app
+subprocess.run(["streamlit", "run", script_path])
